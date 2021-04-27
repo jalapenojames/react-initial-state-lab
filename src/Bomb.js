3d180538eebd338089d0f5917adac982eaa84fb4
James <jaames.chen@gmail.com>
2021-04-27 09:49:32 -0400

    Completed initial state lab

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,18 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+
+    constructor(prop) {
+        super(prop)
+        this.state = {
+            secondsLeft: prop.initialCount
+        }
+    }
+
+    render() {
+        return (
+            this.state.secondsLeft === 0? <div>Boom!</div> : <div>{this.state.secondsLeft} seconds left before I go boom!</div>
+        )
+    }
+
+}
